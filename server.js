5a77c67e288d0b8170d821cabc482a61d46c8b40
Daniel Duran Jr <danielduranjr@Daniels-Air.attlocal.net>
2020-06-06 17:41:26 -0500

    added api and html routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-const db =  require("./models");
+const db = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,10 +10,71 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
-app.listen(PORT,()=>{console.log(`App running on port ${PORT}`)});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.post("/api/workouts", (req, res) => {
+  req.body.day = Date.now();
+  db.Workout.create(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.put("/api/workouts/:id"),
+  (req, res) => {
+    db.Workout.update(
+      {
+        _id: req.params.id,
+      },
+      {
+        $push: {
+          exercises: req.body,
+        },
+
+        $inc: {
+          totalDuration: req.body.duration,
+        },
+      },
+      {
+        new: true,
+      }
+    )
+      .then((dbExercise) => {
+        res.json(dbExercise);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  };
+
+  app.get("/stats", function(req, res){
+      res.sendFile(path.join(__dirname, "./public/stats.html"))
+  });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
